fc2b5b539307295d3a2a5ef5910c58283eb5a0f9
Praneet <pranitgawari@gmail.com>
2024-03-29 09:43:15 +0530

    class school added

--- src/main/java/PodTest.java
+++
@@ -1,9 +0,0 @@
-public class PodTest {
-
-
-    public static void main(String [] args){
-
-        System.out.println("Jay Ganesh");
-
-    }
-}
---
+++ src/main/java/School.java
@@ -0,0 +1,14 @@
+public class School {
+
+    String name;
+    Srting address;
+    String principle;
+    int teacherCount;
+
+    int studentcount;
+
+    public void displayResult()
+    {
+        System.out.println("Every student passed with flying colors");
+    }
+}
